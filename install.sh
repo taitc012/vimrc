ec8887285567d49386d9bd64ee3586b559f89e88
taitc <taitc@cs.nctu.edu.tw>
2015-09-18 02:13:27 +0800

    add gitignore,modify ln in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!env sh -x
 ln -s ~/vimfile ~/.vim
-ln -s ~/vimfile/.vimrc ~/.vimrc
+ln -s ~/.vim/.vimrc ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
